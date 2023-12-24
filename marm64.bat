66a84886a9d5479c01bba769aa8de8780f4608cb
davidly <davidly_lee@hotmail.com>
2023-12-23 21:07:29 -0800

    fix arm64 build script

--- marm64.bat
+++ marm64.bat
@@ -3,6 +3,6 @@ del rvos.exe
 del rvos.pdb
 @echo on
 
-rem cl /nologo rvos.cxx riscv.cxx /MT /Ot /Ox /Ob2 /Oi /Qpar /O2 /EHac /Zi /DNDEBUG /link ntdll.lib /OPT:REF
-cl /nologo rvos.cxx riscv.cxx /MT /Ot /Ox /Ob2 /Oi /Qpar /O2 /EHac /Zi /DDEBUG /link ntdll.lib user32.lib /OPT:REF
+rem cl /nologo rvos.cxx riscv.cxx /I . /MT /Ot /Ox /Ob2 /Oi /Qpar /O2 /EHac /Zi /DNDEBUG /link ntdll.lib /OPT:REF
+cl /nologo rvos.cxx riscv.cxx /I . /MT /Ot /Ox /Ob2 /Oi /Qpar /O2 /EHac /Zi /DDEBUG /link ntdll.lib user32.lib /OPT:REF
 
