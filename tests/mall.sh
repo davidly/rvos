c3a78efd86157278c52798e698ebfaf1a51cb42c
davidly <davidly_lee@hotmail.com>
2023-12-27 13:09:21 -0800

    add test case

--- tests/mall.sh
+++ tests/mall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for arg in t tenv ts tf tm tap ttt sieve e an tdir tpi td ba mysort tphi ttime terrno;
+for arg in t glob tenv ts tf tm tap ttt sieve e an tdir tpi td ba mysort tphi ttime terrno;
 do
     echo $arg
     mt.sh "$arg"
