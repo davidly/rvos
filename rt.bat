047efea7ec36737afc9137fcf6e7f15d0896dc78
davidly <davidly_lee@hotmail.com>
2024-02-12 15:18:20 -0800

    enable nested rvos in test scripts

--- rt.bat
+++ rt.bat
@@ -1,10 +1,13 @@
 @echo off
 setlocal
 
+rem any command-line argument means run rvos nested in rvos
+if "%1" == "" (set _rvoscmd=rvos) else (set _rvoscmd=rvos -h:60 linux\rvos)
+
 set outputfile=test_rvos.txt
 echo %date% %time% >"%outputfile%"
 
-set _testlist=t glob ts sieve e tap tpi tphi tins terrno tp1k ttt^
+set _testlist=t glob ts sieve e tap tpi tphi tins terrno tp ttt^
               tf tm ttime td fileops t_setjmp tex trw empty
 
 ( for %%t in (%_testlist%) do ( call :testRun %%t ) )
@@ -15,11 +18,11 @@ goto :rustTests
 
 echo running test app %~1 using brk heap >>"%outputfile%"
 rem using brk heap, so old gcc's libc is ok
-rvos -h:10 -m:0 linux\%~1 >>"%outputfile%"
+%_rvoscmd% -h:10 -m:0 linux\%~1 >>"%outputfile%"
 
 echo running test app %~1 using mmap heap >>"%outputfile%"
 rem using mmap heap, so a newer libc is required to not require brk
-rvos -h:0 -m:10 linux\%~1 >>"%outputfile%"
+%_rvoscmd% -h:0 -m:10 linux\%~1 >>"%outputfile%"
 
 exit /b 0
 
@@ -36,60 +39,71 @@ goto :singletonTests
 :testRunRust
 
 echo running test app %~1 using brk heap >>"%outputfile%"
-rvos -h:10 -m:0 debianrv\rust\%~1 >>"%outputfile%"
+%_rvoscmd% -h:10 -m:0 debianrv\rust\%~1 >>"%outputfile%"
 
 echo running test app %~1 using mmap >>"%outputfile%"
-rvos -h:0 -m:10 debianrv\rust\%~1 >>"%outputfile%"
+%_rvoscmd% -h:0 -m:10 debianrv\rust\%~1 >>"%outputfile%"
 
 exit /b 0
 
 :singletonTests
 
-rvos -h:0 -m:20 debianrv\tmmap >>%outputfile%
+echo running ttt_riscv >>%outputfile%
+%_rvoscmd% -p ttt_riscv.elf 1 >>%outputfile%
+
+echo running ttt_rvu >>%outputfile%
+%_rvoscmd% -p ttt_rvu.elf 1 >>%outputfile%
+
+echo running tmmap >>%outputfile%
+%_rvoscmd% -h:0 -m:20 debianrv\tmmap >>%outputfile%
 
 echo running tcrash memory low >>%outputfile%
-rvos linux\tcrash ml >>%outputfile%
+%_rvoscmd% linux\tcrash ml >>%outputfile%
 
 echo running tcrash memory high >>%outputfile%
-rvos linux\tcrash mh >>%outputfile%
+%_rvoscmd% linux\tcrash mh >>%outputfile%
 
 echo running tcrash pc low >>%outputfile%
-rvos linux\tcrash pcl >>%outputfile%
+%_rvoscmd% linux\tcrash pcl >>%outputfile%
 
 echo running tcrash pc high >>%outputfile%
-rvos linux\tcrash pch >>%outputfile%
+%_rvoscmd% linux\tcrash pch >>%outputfile%
 
 echo running tcrash stack pointer low >>%outputfile%
-rvos linux\tcrash spl >>%outputfile%
+%_rvoscmd% linux\tcrash spl >>%outputfile%
 
 echo running tcrash stack pointer l=high >>%outputfile%
-rvos linux\tcrash sph >>%outputfile%
+%_rvoscmd% linux\tcrash sph >>%outputfile%
 
 echo running tcrash stack pointer misaligned >>%outputfile%
-rvos linux\tcrash spm >>%outputfile%
+%_rvoscmd% linux\tcrash spm >>%outputfile%
 
 echo running ba tp.bas >>%outputfile%
-rvos linux\ba linux\tp.bas >>%outputfile% >>%outputfile%
+%_rvoscmd% linux\ba linux\tp.bas >>%outputfile% >>%outputfile%
 
 echo running ttty tests >>%outputfile%
 echo blahblah >bar
-rvos linux\ttty >>%outputfile%
-rvos linux\ttty <bar >>%outputfile%
+%_rvoscmd% linux\ttty >>%outputfile%
+%_rvoscmd% linux\ttty <bar >>%outputfile%
 echo should see no, yes, yes, yes
-rvos linux\ttty <bar 
-rvos linux\ttty
+%_rvoscmd% linux\ttty <bar 
+%_rvoscmd% linux\ttty
 
 echo running mysort >>%outputfile%
 del linux\sorted.txt
-rvos /h:2 linux\mysort /q /u linux\words.txt linux\sorted.txt >>%outputfile%
+%_rvoscmd% -h:2 linux\mysort /q /u linux\words.txt linux\sorted.txt >>%outputfile%
 head linux\sorted.txt >>%outputfile%
 
 echo running an with brk>>%outputfile%
-rvos -h:40 -m:0 linux\an david lee >>%outputfile%
+%_rvoscmd% -h:40 -m:0 linux\an david lee >>%outputfile%
 
 echo running an with mmap >>%outputfile%
-rvos -h:0 -m:40 debianrv\an phoebe bridgers >>%outputfile%
+%_rvoscmd% -h:0 -m:40 debianrv\an phoebe bridgers >>%outputfile%
 
+echo running ntvao, ntvcm, and ntvdm >>%outputfile%
+%_rvoscmd% -h:40 linux\ntvao -c -p ttt1.hex >>%outputfile%
+%_rvoscmd% -h:40 linux\ntvcm -c -p tttcpm.com 1 >>%outputfile%
+%_rvoscmd% -h:40 linux\ntvdm -c -p ttt8086.com 1 >>%outputfile%
+
+echo %date% %time% >>"%outputfile%"
 diff baseline_%outputfile% %outputfile%
-
-:eof
--- rtdrv.bat
+++ rtdrv.bat
@@ -1,12 +1,15 @@
 @echo off
 setlocal
 
+rem any command-line argument means run rvos nested in rvos
+if "%1" == "" (set _rvoscmd=rvos) else (set _rvoscmd=rvos -h:60 debianrv\rvos)
+
 set outputfile=testdrv_rvos.txt
 echo %date% %time% >"%outputfile%"
 
 robocopy \\lee-server\documents\scratch\debianrv debianrv * /s >>%outputfile%
 
-set _testlist=t glob ts sieve e tap tpi tphi tins terrno tp1k ttt tf^
+set _testlist=t glob ts sieve e tap tpi tphi tins terrno tp ttt tf^
               tm ttime td fileops t_setjmp tex trw empty
 
 ( for %%t in (%_testlist%) do ( call :testRun %%t ) )
@@ -17,11 +20,11 @@ goto :runRustTests
 
 echo running test app %~1 using brk heap >>"%outputfile%"
 rem using brk heap, so old gcc's libc is ok
-rvos -h:10 -m:0 debianrv\%~1 >>"%outputfile%"
+%_rvoscmd% -h:10 -m:0 debianrv\%~1 >>"%outputfile%"
 
 echo running test app %~1 using mmap heap >>"%outputfile%"
 rem using mmap heap, so a newer libc is required to not require brk
-rvos -h:0 -m:10 debianrv\%~1 >>"%outputfile%"
+%_rvoscmd% -h:0 -m:10 debianrv\%~1 >>"%outputfile%"
 
 exit /b 0
 
@@ -36,41 +39,48 @@ goto :runSingletonTests
 :testRunRust
 
 echo running test app %~1 using brk heap >>"%outputfile%"
-rvos -h:10 -m:0 debianrv\rust\%~1 >>"%outputfile%"
+%_rvoscmd% -h:10 -m:0 debianrv\rust\%~1 >>"%outputfile%"
 
 echo running test app %~1 using mmap >>"%outputfile%"
-rvos -h:0 -m:10 debianrv\rust\%~1 >>"%outputfile%"
+%_rvoscmd% -h:0 -m:10 debianrv\rust\%~1 >>"%outputfile%"
 
 exit /b 0
 
 :runSingletonTests
 
-rvos -h:0 -m:20 debianrv\tmmap >>%outputfile%
+echo running tmmap >>%outputfile%
+%_rvoscmd% -h:0 -m:20 debianrv\tmmap >>%outputfile%
 
-rvos debianrv\tcrash ml >>%outputfile%
-rvos debianrv\tcrash mh >>%outputfile%
-rvos debianrv\tcrash pcl >>%outputfile%
-rvos debianrv\tcrash pch >>%outputfile%
-rvos debianrv\tcrash spl >>%outputfile%
-rvos debianrv\tcrash sph >>%outputfile%
-rvos debianrv\tcrash spm >>%outputfile%
+%_rvoscmd% debianrv\tcrash ml >>%outputfile%
+%_rvoscmd% debianrv\tcrash mh >>%outputfile%
+%_rvoscmd% debianrv\tcrash pcl >>%outputfile%
+%_rvoscmd% debianrv\tcrash pch >>%outputfile%
+%_rvoscmd% debianrv\tcrash spl >>%outputfile%
+%_rvoscmd% debianrv\tcrash sph >>%outputfile%
+%_rvoscmd% debianrv\tcrash spm >>%outputfile%
 
 echo running ttty tests >>%outputfile%
 echo blahblah >bar
-rvos debianrv\ttty >>%outputfile%
-rvos debianrv\ttty <bar >>%outputfile%
+%_rvoscmd% debianrv\ttty >>%outputfile%
+%_rvoscmd% debianrv\ttty <bar >>%outputfile%
 echo should see no, yes, yes, yes
-rvos debianrv\ttty <bar 
-rvos debianrv\ttty
+%_rvoscmd% debianrv\ttty <bar 
+%_rvoscmd% debianrv\ttty
 
-rvos debianrv\ba debianrv\tp.bas >>%outputfile%
+%_rvoscmd% debianrv\ba debianrv\tp.bas >>%outputfile%
 
-rvos /h:2 debianrv\mysort /q /u debianrv\words.txt debianrv\sorted.txt >>%outputfile%
-rvos /h:40 debianrv\an david lee >>%outputfile%
+%_rvoscmd% -h:2 debianrv\mysort /q /u debianrv\words.txt debianrv\sorted.txt >>%outputfile%
+%_rvoscmd% -h:40 debianrv\an david lee >>%outputfile%
 
-rvos -h:30 debianrv\rvos debianrv\tp.elf 1 >>%outputfile%
-rvos -h:30 debianrv\ntvao -c ../ntvao/ttt1.hex >>%outputfile%
-rvos -h:30 debianrv\ntvcm -c ../ntvcm/tttcpm.com 1 >>%outputfile%
-rvos -h:30 debianrv\ntvdm -c ../ntvdm/ttt8086.com 1 >>%outputfile%
+%_rvoscmd% -h:30 debianrv\rvos debianrv\tp.elf 1 >>%outputfile%
+%_rvoscmd% -h:30 debianrv\ntvao -c ../ntvao/ttt1.hex >>%outputfile%
+%_rvoscmd% -h:30 debianrv\ntvcm -c ../ntvcm/tttcpm.com 1 >>%outputfile%
+%_rvoscmd% -h:30 debianrv\ntvdm -c ../ntvdm/ttt8086.com 1 >>%outputfile%
 
+echo running ntvao, ntvcm, and ntvdm >>%outputfile%
+%_rvoscmd% -h:40 debianrv\ntvao -c -p ttt1.hex >>%outputfile%
+%_rvoscmd% -h:40 debianrv\ntvcm -c -p tttcpm.com 1 >>%outputfile%
+%_rvoscmd% -h:40 debianrv\ntvdm -c -p ttt8086.com 1 >>%outputfile%
+
+echo %date% %time% >>"%outputfile%"
 diff baseline_%outputfile% %outputfile%
