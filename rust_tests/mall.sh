62f1c4fce2e6c4fd39db7a449309550ad988feaa
davidly <davidly_lee@hotmail.com>
2025-03-20 11:51:44 -0700

    turn off overflow checks for rust tests

--- rust_tests/mall.sh
+++ rust_tests/mall.sh
@@ -8,7 +8,7 @@ do
     for optflag in 0 1 2 3;
     do
         mkdir bin"$optflag" 2>/dev/null
-        rustc --edition 2021 --out-dir bin"$optflag" -C opt-level="$optflag" -C target-feature=+crt-static "$arg".rs
+        rustc --edition 2021 --out-dir bin"$optflag" -C overflow-checks=off -C opt-level="$optflag" -C target-feature=+crt-static "$arg".rs
     done    
 done
 
