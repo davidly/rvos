3701a0083904fab203b92d2d89f33f649273f67f
davidly <davidly_lee@hotmail.com>
2025-03-22 07:57:08 -0700

    build tests in parallel

--- c_tests/mallx.sh
+++ c_tests/mallx.sh
@@ -8,7 +8,13 @@ do
     for optflag in 0 1 2 3 fast;
     do
         mkdir bin"$optflag" 2>/dev/null
-        riscv64-unknown-linux-gnu-c++ "$arg".c -o bin"$optflag"/"$arg" -O"$optflag" -mcmodel=medany -mabi=lp64d -march=rv64imadc -latomic -static -fsigned-char -Wno-format -Wno-format-security
+        _gnubuild="riscv64-unknown-linux-gnu-c++ "$arg".c -o bin"$optflag"/"$arg" -O"$optflag" -mcmodel=medany -mabi=lp64d -march=rv64imadc -latomic -static -fsigned-char -Wno-format -Wno-format-security"
+
+        if [ "$optflag" != "fast" ]; then
+            $_gnubuild &
+        else    
+            $_gnubuild
+        fi
     done
 done
 
